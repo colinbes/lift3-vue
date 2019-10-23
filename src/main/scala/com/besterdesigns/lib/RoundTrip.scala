e0249562d111c1f2b27be1ca8079c6a54f138547
Colin Bester <bester.colin@gmail.com>
2019-10-23 13:38:31 -0500

    Added services injection. Very basic code, needs lots of work

--- src/main/scala/com/besterdesigns/lib/RoundTrip.scala
+++ src/main/scala/com/besterdesigns/lib/RoundTrip.scala
@@ -1,8 +1,13 @@
 package com.besterdesigns.lib
 
+import net.liftweb.common.Empty
 import net.liftweb.http.RoundTripInfo
 import net.liftweb.http.S
 import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.JsCmds
+import net.liftweb.http.js.JsCmds._
+
+import scala.xml.NodeSeq
 
 abstract class PageRoundTrips {
   protected def getRoundTrips : List[RoundTripInfo]
@@ -10,13 +15,31 @@ abstract class PageRoundTrips {
  
 class EmptyRoundTrip extends PageRoundTrips {
   protected def getRoundTrips : List[RoundTripInfo] = Nil
-  
+
+  /**
+   * Return roundtrip functions referenced by services name
+   * @return NodeSeq
+   */
+  def services(serviceName: String): NodeSeq = {
+    val res = for {
+      session <- S.session
+    } yield {
+      val rt = session.buildRoundtrip(getRoundTrips).toJsCmd
+      val data = s"var $serviceName=${rt}"
+      Script(JsRaw(data).cmd)
+    }
+    res.getOrElse(Script())
+  }
+
+  /**
+   * Insert roundtrip in vue angular space.
+   * @param functionName
+   */
   def addServices(functionName: String) = {
     for {
       session <- S.session
     } {
         val data = s"""window.vue.$functionName = ${session.buildRoundtrip(getRoundTrips).toJsCmd}"""
-        println(s"inserting data $data")
         S.appendGlobalJs(JsRaw(data).cmd)
     }
   }
